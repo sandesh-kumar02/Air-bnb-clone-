cba2e22f4c27ee691339d36bbc70b69b1544afd4
sandesh-kumar02 <sandeshkumar98014f@gmail.com>
2026-04-05 07:57:11 +0530

    My Airbnb-clone project

--- models/User.js
+++ models/User.js
@@ -2,12 +2,24 @@ import mongoose from "mongoose";
 import pkg from "passport-local-mongoose";
 const passportLocalMongoose = pkg;
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const plugin = passportLocalMongoose.default || passportLocalMongoose;
 userSchema.plugin(plugin);
